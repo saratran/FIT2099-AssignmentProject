69153cfc22072944b88e2714aa52c7dfb06b5202
Sara Uyen Tran <utra0001@student.monash.edu>
2019-09-08 17:58:10 +1000

    Grow tree nearby

--- src/game/Tree.java
+++ src/game/Tree.java
@@ -1,10 +1,12 @@
 package game;
 
+import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
 
 public class Tree extends Ground {
 	private int age = 0;
+	private double GROW_NEW_TREE = 0.005;
 
 	public Tree() {
 		super('+');
@@ -13,6 +15,26 @@ public class Tree extends Ground {
 	@Override
 	public void tick(Location location) {
 		super.tick(location);
+
+		int x = location.x();
+		int y = location.y();
+		GameMap map = location.map();
+		
+		// Go through surrounding locations
+		for (int m = x - 1; m <= x + 1; m++) {
+			for (int n = y - 1; n <= y + 1; n++) {
+				// Is it a good idea to use an empty try-catch block for out-of-bound cases?
+				try {
+					// TODO: what if ground is wall or something else
+					if (!map.at(m, n).getGround().getClass().equals(Tree.class)) {
+						if (Math.random() <= this.GROW_NEW_TREE) {
+							map.at(m, n).setGround(new Tree());
+						}
+					}
+				} catch (Exception e) {
+				}
+			}
+		}
 
 		age++;
 		if (age == 10)
