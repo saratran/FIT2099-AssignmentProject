8012943624451dc32a24813fcb60db268f8b9d28
Sara Uyen Tran <utra0001@student.monash.edu>
2019-09-12 15:13:48 +1000

    Add Egg class
    Can hatch into protoceratops using enum and switch case. May not be very extensible.

---
+++ src/game/Egg.java
@@ -0,0 +1,30 @@
+package game;
+
+import edu.monash.fit2099.engine.Location;
+
+public class Egg extends PortableDinoItem {
+	private int age = 0;
+	private int hatch_age = 10;
+	private Species species;
+
+	
+	public Egg(String name, char displayChar, Species species) {
+		super(name, displayChar);
+		this.species = species;
+	}
+
+	@Override
+	public void tick(Location currentLocation) {
+		age++;
+		if(age >= hatch_age) {
+			switch(species) {
+			case PROTOCERATOPS:
+				currentLocation.addActor(new Protoceratops("Protoceratops"));
+				currentLocation.removeItem(this);
+			}
+		}
+	}
+	
+	
+
+}
