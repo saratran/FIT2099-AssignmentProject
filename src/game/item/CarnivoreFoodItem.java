2043a10ea51d9e2fcb216acbef69183fb01fecb4
Sara Uyen Tran <utra0001@student.monash.edu>
2019-09-28 12:48:39 +1000

    Modify how dino keeps track of edible food

--- src/game/item/CarnivoreFoodItem.java
+++ src/game/item/CarnivoreFoodItem.java
@@ -1,9 +1,12 @@
 package game.item;
 
+import game.FoodSkill;
+
 public class CarnivoreFoodItem extends FoodItem {
 
 	public CarnivoreFoodItem(String name, char displayChar) {
 		super(name, displayChar);
+		addSkill(FoodSkill.CARNIVORE);
 	}
 
 }
