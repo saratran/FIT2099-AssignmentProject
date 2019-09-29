5b44cc0257217df85fa027af25ffd5b302f6843f
Sara Uyen Tran <utra0001@student.monash.edu>
2019-09-29 13:49:13 +1000

    Add EdibleGroundInterface

--- src/game/action/EatGroundAction.java
+++ src/game/action/EatGroundAction.java
@@ -5,6 +5,7 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
+import edu.monash.fit2099.interfaces.EdibleGroundInterface;
 import edu.monash.fit2099.interfaces.EdibleInterface;
 import game.dinosaur.Dinosaur;
 import game.ground.Dirt;
@@ -19,13 +20,9 @@ public class EatGroundAction extends EatAction {
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
-//		if (actor instanceof Dinosaur) {
-//			((Dinosaur) actor).addFoodValue(((EdibleInterface)target).getFoodValue());
-//		}
-
 		// TODO: Better way to set new ground
 		// EdibleGroundInterface
-		location.setGround(new Dirt());
+		location.setGround(((EdibleGroundInterface)food).eatenGround());
 		return super.execute(actor, map);
 	}
 
