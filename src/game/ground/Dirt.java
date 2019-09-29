b407b032bc481455c2f25d998f397e86e0914b15
Harun Ergi <herg0001@student.monash.edu>
2019-09-29 17:17:39 +1000

    Added Dirt Growing Grass

--- src/game/ground/Dirt.java
+++ src/game/ground/Dirt.java
@@ -1,13 +1,24 @@
 package game.ground;
 
-import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.*;
 
 /**
  * A class that represents bare dirt.
  */
 public class Dirt extends Ground {
 
+	private final double Grass_Growth_Chance = 0.001;
 	public Dirt() {
 		super('.');
+	}
+	
+	public void Tick(Location location) {
+		GrowGrass(location);
+	}
+	
+	public void GrowGrass(Location location) {
+		if (Math.random() < Grass_Growth_Chance){
+			location.setGround(new Grass());
+		}
 	}
 }
