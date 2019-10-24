123bf619b1a9c9cba5c44abd0fd3a0a5ac0d42d1
Sara Uyen Tran <utra0001@student.monash.edu>
2019-10-24 15:34:05 +1100

    Add support for map expansion in all 4 directions

--- src/game/DinoGameMap.java
+++ src/game/DinoGameMap.java
@@ -20,38 +20,56 @@ public class DinoGameMap extends GameMap {
 		super(groundFactory, lines);
 	}
 
-	// TODO: other directions, also need to handle case when map sizes are different
 	public void addAdjacentMap(GameMap map, Direction direction) {
 		adjacentMaps.put(direction, map);
-		if(direction == Direction.NORTH) {
-			for(int x=0; x<=getXRange().max(); x++) {
-				Location destination = map.at(x, map.getYRange().max());
-				this.at(x,0).addExit(new Exit("North", destination, "8"));
-			}
-		}
-		if(direction == Direction.SOUTH) {
-			for(int x=0; x<=getXRange().max(); x++) {
-				Location destination;
-				if (map.getXRange().contains(x)) {
-					destination = map.at(x, 0);
-					int y = getYRange().max();
-					this.at(x,y).addExit(new Exit("South", destination, "2"));
-				}
-				if (map.getXRange().max() < this.getXRange().max()) {
-					if (x < map.getXRange().min()){
-						destination = map.at(map.getXRange().min(), 0);
-					} else if (x > map.getXRange().max()) {
-						destination = map.at(map.getXRange().max(), 0);
-					} else {
-						destination = map.at(x, 0);
-					}
-					int y = getYRange().max();
-					this.at(x,y).addExit(new Exit("South", destination, "2"));
-				}
-	
-			}
-		}
-	}
+		int maxX = Math.min(getXRange().max(), map.getXRange().max());
+		int maxY = Math.min(getYRange().max(), map.getYRange().max());
 
+		if (direction == Direction.NORTH) {
+			for (int x = 0; x <= maxX; x++) {
+				Location destination = map.at(x, map.getYRange().max());
+				this.at(x, 0).addExit(new Exit("North", destination, "8"));
+			}
+		} else if (direction == Direction.SOUTH) {
+			for (int x = 0; x <= maxX; x++) {
+				Location destination = map.at(x, map.getYRange().min());
+				this.at(x, getYRange().max()).addExit(new Exit("South", destination, "2"));
+			}
+		} else if (direction == Direction.WEST) {
+			for (int y = 0; y <= maxY; y++) {
+				Location destination = map.at(map.getXRange().min(), y);
+				this.at(getXRange().max(), y).addExit(new Exit("West", destination, "6"));
+			}
+		} else if (direction == Direction.EAST) {
+			for (int y = 0; y <= maxY; y++) {
+				Location destination = map.at(map.getXRange().max(), y);
+				this.at(getXRange().min(), y).addExit(new Exit("East", destination, "4"));
+			}
+		}
+		
+		// this works but expanding may get a little messy
+		// the method above just ignore the locations that exceed the limit
+//		if (direction == Direction.SOUTH) {
+//			for (int x = 0; x <= getXRange().max(); x++) {
+//				Location destination;
+//				if (map.getXRange().contains(x)) {
+//					destination = map.at(x, 0);
+//					int y = getYRange().max();
+//					this.at(x, y).addExit(new Exit("South", destination, "2"));
+//				}
+//				if (map.getXRange().max() < this.getXRange().max()) {
+//					if (x < map.getXRange().min()) {
+//						destination = map.at(map.getXRange().min(), 0);
+//					} else if (x > map.getXRange().max()) {
+//						destination = map.at(map.getXRange().max(), 0);
+//					} else {
+//						destination = map.at(x, 0);
+//					}
+//					int y = getYRange().max();
+//					this.at(x, y).addExit(new Exit("South", destination, "2"));
+//				}
+//			}
+//		}
+	}
 
 }
